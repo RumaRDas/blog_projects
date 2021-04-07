b0093463b41acd6956f814f37fb0cff63a8f0363
Ruma Das <rumaadl16@gmail.com>
2021-04-07 19:24:27 +0930

    step =09: added users component to display all users in a material table with paginator and added dropdown or select to the menubar

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { AuthenticationService } from './../../services/authentication.service';
+import { AuthenticationService } from '../../services/authentication-service/authentication.service';
 
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
